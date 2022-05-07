e470ac45c82114eaada0543e438b8db44021d101
Diego Amicabile <diego.amicabile@gmail.com>
2022-05-07 22:43:34 +0200

    Mlproject run

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -10,10 +10,23 @@ logger = logging.getLogger()
 
 
 def go(args):
-
     run = wandb.init(project="exercise_5", job_type="process_data")
+    artifact = run.use_artifact(args.input_artifact)
+    df = pd.read_parquet(artifact.file())
+    df = df.drop_duplicates().reset_index(drop=True)
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+    df.to_csv('genres_mod_new.csv')
 
-    pass
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description,
+    )
+    artifact.add_file("genres_mod_new.csv")
+    run.log_artifact(artifact)
+
 
 
 if __name__ == "__main__":
